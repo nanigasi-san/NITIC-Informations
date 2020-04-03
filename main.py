6ada81a7da75751e93404a41c5414c9d80b6b9fa
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-04-03 14:50:15 +0900

    [change]scheduleライブラリを使用
    scheduleライブラリを使って毎分メール受信の確認、新着メールがあった場合LINEへ通知するように変更

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import schedule
+import time
+
 import receive_gmail
 import notifying_to_LINE
 import my_info
 
-message = receive_gmail.get_gmali_information(my_info.sender_addresses)
-if message != "No new mail":
-    notifying_to_LINE.notifying(message)
+def main_action():
+    try:
+        message = receive_gmail.get_gmali_information(my_info.sender_addresses)
+        if message != "No new mail":
+            notifying_to_LINE.notifying(message)
+    except:
+        print("any error occurred...")
+
+schedule.every(1).minutes.do(main_action)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
