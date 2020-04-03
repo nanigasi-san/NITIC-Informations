62aeaecb12e223babcd8c2594a9ca142db849a45
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-04-03 14:33:44 +0900

    [update]動作確認を追加
    各関数の動作確認を追加

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import receive_gmail
 import notifying_to_LINE
+import my_info
 
+message = receive_gmail.get_gmali_information(my_info.sender_addresses)
+if message != "No new mail":
+    notifying_to_LINE.notifying(message)
