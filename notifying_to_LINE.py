3e194004a4da523959d29dbf989609b1284cff91
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-04-03 11:18:10 +0900

    [add]LINEへ通知する関数
    LINEへ文章と画像を通知する関数を作成（写経）、テストをして動作確認済み

---
+++ notifying_to_LINE.py
@@ -0,0 +1,27 @@
+import argparse
+import requests
+
+def notifying(message, image_path="No image"):
+    """
+    LINEへ通知する
+    参考: https://kuroyagikun.com/python-line-message-picture-send/
+
+    Parameters
+    ----------
+    message : string
+        通知する文章。
+    image_path : string
+        通知する画像の絶対パス。
+    """
+
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    line_notify_token = "write your token"
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    payload = {'message': message}
+    if image_path == "No image":
+        requests.post(line_notify_api, data=payload, headers=headers)
+    else:
+        files = {"imageFile": open(image_path, "rb")}
+        requests.post(line_notify_api, data=payload, headers=headers, files=files)
+
+notifying(message="test", image_path="C:\\Users\\hoge\\Pictures\\Screenshots\\me.png")
