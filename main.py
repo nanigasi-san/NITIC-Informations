01985aff7bd6a3616c021706760951551438cb7b
yuina-blend <moon.h14.dd2.as@gmail.com>
2020-04-03 16:03:01 +0900

    [fix]fix typo

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ import notifying_to_LINE
 import my_info
 
 def main_action():
-    try:
-        message = receive_gmail.get_gmali_information(my_info.sender_addresses)
-        if message != "No new mail":
-            notifying_to_LINE.notifying(message)
-    except:
-        print("any error occurred...")
+    message = receive_gmail.get_gmali_information(my_info.block_addresses)
+    if message != "No new mail":
+        notifying_to_LINE.notifying(message)
 
 schedule.every(1).minutes.do(main_action)
 
